e0c60a6344a72a020826245bef05ad0747be0394
wavesj <wavesj04@gmail.com>
2025-06-26 16:36:29 -0700

    Add hover and click color change

--- js-file.js
+++ js-file.js
@@ -4,4 +4,15 @@ for (let i = 0; i < 16*16; i++) {
     container.appendChild(document.createElement("div"));
 }
 
+const boxes = document.querySelectorAll(".container div")
+
+boxes.forEach((box) => {
+    box.addEventListener("click", (event) => {
+        box.style["background-color"] = "red";
+    });
+    box.addEventListener("mouseenter", (event) => {
+        box.style.backgroundColor = "green";
+    });
+})
+
 

ff940ac10d604e54952122a7442114bea9d2ebb3
wavesj <wavesj04@gmail.com>
2025-06-26 17:55:56 -0700

    Added change size button

--- js-file.js
+++ js-file.js
@@ -1,12 +1,30 @@
 const container = document.querySelector(".container");
+const reset = document.querySelector("button");
+
+
+reset.addEventListener("click", (event) => resetDimension(changeLength()));
 
 for (let i = 0; i < 16*16; i++) {
     container.appendChild(document.createElement("div"));
 }
 
-const boxes = document.querySelectorAll(".container div")
+let boxes = document.querySelectorAll(".container div")
 
-boxes.forEach((box) => {
+changePallete();
+
+function changeLength() {
+    let length = -1;
+    while (true) {
+        length = Number(prompt("Enter length (1-100)", 1));
+        if (length > 0 && length <= 100) {
+            break;
+        }
+    }
+    return length;
+}
+
+function changePallete() {
+    boxes.forEach((box) => {
     box.addEventListener("click", (event) => {
         box.style["background-color"] = "red";
     });
@@ -14,5 +32,25 @@ boxes.forEach((box) => {
         box.style.backgroundColor = "green";
     });
 })
+}
 
+function resetDimension(dimension) {
+    boxes.forEach((box) => {
+        container.removeChild(box);
+    })
+
+    for (let i = 0; i < dimension*dimension; i++) {
+        container.appendChild(document.createElement("div"));
+    }
+
+    boxes = document.querySelectorAll(".container div")
+    boxes.forEach((box) => {
+        box.style.width = 800/dimension + "px";
+        box.style.height = 800/dimension + "px";
+        box.style.backgroundColor = "aqua";
+    })
+
+    changePallete();
+
+}
 
